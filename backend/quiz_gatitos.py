4f1ec7b5c1338a929117552d757aa1f2d14cc091
AllenGuitar <sarita.sotaquira@gmail.com>
2026-06-23 13:14:21 -0500

    feat: agregar workflow CI y pruebas pytest

--- backend/quiz_gatitos.py
+++ backend/quiz_gatitos.py
@@ -44,37 +44,3 @@ def nivel_michi(puntaje):
         return "Aprendiz"
     else:
         return "Necesitas pasar más tiempo con michis :("
-
-#Validacion de testeos en base al programa
-
-from quiz_gatitos import verificar_respuesta, calcular_puntaje, nivel_michi, obtener_pregunta
-
-def test_frap_correcto():
-    assert verificar_respuesta(1, "frenetic random activity period") == True
-
-def test_frap_incorrecto():
-    assert verificar_respuesta(1, "free running animal period") == False
-
-def test_ignora_mayusculas():
-    assert verificar_respuesta(1, "Frenetic Random Activity Period") == True
-
-def test_puntaje_perfecto():
-    respuestas = {
-        1: "frenetic random activity period",
-        2: "16",
-        3: "organo de jacobson",
-        4: "48"
-    }
-    assert calcular_puntaje(respuestas) == 4
-
-def test_puntaje_parcial():
-    respuestas = {
-        1: "frenetic random activity period",
-        2: "8",
-        3: "organo de jacobson",
-        4: "manada"
-    }
-    assert calcular_puntaje(respuestas) == 2
-
-def test_nivel_experto():
-    assert nivel_gatero(4) == "Experto en michis"
